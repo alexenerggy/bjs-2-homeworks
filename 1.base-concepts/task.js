4b1f2a257ab0dcd95642138ac1fd8d035203ae0a
Alex Volni <ya.volnitck@yandex.ru>
2024-09-30 20:23:00 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,26 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discriminant = b ** 2 - 4 * a * c;
+
+	if (discriminant < 0) {
+		// Если дискриминант меньше нуля, корней нет, возвращаем пустой массив
+		return arr;
+	} else if (discriminant === 0) {
+		// Если дискриминант равен нулю, один корень
+		let root = -b / (2 * a);
+		arr.push(root); // Добавляем единственный корень в массив
+	} else {
+		// Если дискриминант больше нуля, два корня
+		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(root1, root2); // Добавляем оба корня в массив
+	}
+
+	return arr;
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
